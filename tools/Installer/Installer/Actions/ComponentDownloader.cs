890714a67c21497038a98e9d80d3e49cc38e9ee0
Patrick Morris <pmorris@eagle.org>
2023-07-13 10:49:59 -0700

    Installer shell

---
+++ tools/Installer/Installer/Actions/ComponentDownloader.cs
@@ -0,0 +1,17 @@
+﻿namespace Abs.CommonCore.Installer.Actions
+{
+    public class ComponentDownloader
+    {
+        private readonly FileInfo _registry;
+
+        public ComponentDownloader(FileInfo registry)
+        {
+            _registry = registry;
+        }
+
+        public async Task ExecuteAsync()
+        {
+
+        }
+    }
+}
---
+++ tools/Installer/Installer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.CommandLine;
+using System.Diagnostics.CodeAnalysis;
+using Abs.CommonCore.Installer.Actions;
+
+namespace Abs.CommonCore.Installer
+{
+    [ExcludeFromCodeCoverage]
+    internal class Program
+    {
+        static async Task<int> Main(string[] args)
+        {
+            var downloadCommand = new Command("download", "Download components for installation");
+            downloadCommand.TreatUnmatchedTokensAsErrors = true;
+
+            var registryParam = new Option<FileInfo>("--registry", "Installation registry");
+            registryParam.IsRequired = true;
+            registryParam.AddAlias("-r");
+            downloadCommand.Add(registryParam);
+
+            downloadCommand.SetHandler(async (registry) =>
+            {
+                var downloader = new ComponentDownloader(registry);
+                await downloader.ExecuteAsync();
+            }, registryParam);
+
+            var root = new RootCommand("Installer for the Common Core platform");
+            root.TreatUnmatchedTokensAsErrors = true;
+            root.Add(downloadCommand);
+
+            root.SetHandler(async (mode) =>
+            {
+
+            });
+
+            return await root.InvokeAsync(args);
+        }
+    }
+}
