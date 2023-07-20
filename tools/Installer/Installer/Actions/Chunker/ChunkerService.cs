3b943d9569ac62b1c5f49f1e4bd50bee9b377690
Patrick Morris <pmorris@eagle.org>
2023-07-20 11:54:48 -0700

    Added chunker logic

---
+++ tools/Installer/Installer/Actions/Chunker/ChunkerService.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Abs.CommonCore.Installer.Actions.Chunker
+{
+    public class ChunkerService
+    {
+        private readonly ILogger _logger;
+
+        public ChunkerService(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<ChunkerService>();
+        }
+
+        public async Task ChunkFile(FileInfo source, DirectoryInfo destination, int maxSize)
+        {
+            if (source.Exists == false)
+                throw new Exception("Source file does not exist");
+
+            if (source.Length < maxSize)
+                return;
+
+            Directory.CreateDirectory(destination.Name);
+
+            _logger.LogInformation($"Breaking {source.Name} file into chunks");
+            var buffer = new byte[4 * 1024];
+            var currentChunkCount = 1;
+            var currentChunkFile = File.OpenWrite($"{source.FullName}.part{currentChunkCount}");
+
+            using (var stream = source.OpenRead())
+            {
+                var dataRead = int.MaxValue;
+                var dataWritten = 0;
+
+                while (dataRead > 0)
+                {
+                    dataRead = await stream.ReadAsync(buffer);
+                    await currentChunkFile.WriteAsync(buffer, 0, dataRead);
+                    dataWritten += dataRead;
+
+                    if (dataWritten > maxSize)
+                    {
+                        currentChunkFile.Close();
+                        dataWritten = 0;
+                        currentChunkCount++;
+                        currentChunkFile = File.OpenWrite($"{source.FullName}.part{currentChunkCount}");
+                    }
+                }
+            }
+
+            currentChunkFile.Close();
+        }
+
+        public async Task UnchunkFile(DirectoryInfo source, FileInfo destination)
+        {
+            if (source.Exists == false)
+                throw new Exception("Source location doesn't exist");
+
+            var files = Directory.GetFiles(source.FullName, "*.part?")
+                .OrderBy(x =>
+                {
+                    var extension = Path.GetExtension(x).Replace("part", "");
+                    return int.Parse(extension);
+                });
+
+            destination.Delete();
+            using (var destinationStream = destination.OpenWrite())
+            {
+                foreach (var file in files)
+                {
+                    using (var fileStream = File.OpenRead(file))
+                    {
+                        await fileStream.CopyToAsync(destinationStream);
+                    }
+                }
+            }
+        }
+    }
+}
--- tools/Installer/Installer/Program.cs
+++ tools/Installer/Installer/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.CommandLine;
 using System.Diagnostics.CodeAnalysis;
+using Abs.CommonCore.Installer.Actions.Chunker;
 using Abs.CommonCore.Installer.Actions.Downloader;
 using Abs.CommonCore.Installer.Actions.Installer;
 using Abs.CommonCore.Installer.Services;
@@ -15,74 +16,209 @@ namespace Abs.CommonCore.Installer
     {
         public static async Task<int> Main(string[] args)
         {
-            var downloadCommand = new Command("download", "Download components for installation");
-            downloadCommand.TreatUnmatchedTokensAsErrors = true;
-
-            var installCommand = new Command("install", "Install components");
-            installCommand.TreatUnmatchedTokensAsErrors = true;
-
-            var registryParam = new Option<FileInfo>("--registry", "Location of registry configuration");
-            registryParam.IsRequired = true;
-            registryParam.AddAlias("-r");
-            downloadCommand.Add(registryParam);
-            installCommand.Add(registryParam);
-
-            var downloadConfigParam = new Option<FileInfo>("--download", "Location of download configuration");
-            downloadConfigParam.IsRequired = false;
-            downloadConfigParam.AddAlias("-d");
-            downloadCommand.Add(downloadConfigParam);
-
-            var installConfigParam = new Option<FileInfo>("--install", "Location of install configuration");
-            installConfigParam.IsRequired = false;
-            installConfigParam.AddAlias("-i");
-            installCommand.Add(installConfigParam);
-
-            var componentParam = new Option<string[]>("--component", "Specific component to process");
-            componentParam.IsRequired = false;
-            componentParam.AddAlias("-c");
-            componentParam.AllowMultipleArgumentsPerToken = true;
-            downloadCommand.Add(componentParam);
-            installCommand.Add(componentParam);
-
-            var parameterParam = new Option<string[]>("--parameter", "Specific colon separated key value pair to use as a config parameter");
-            parameterParam.IsRequired = false;
-            parameterParam.AddAlias("-p");
-            parameterParam.AllowMultipleArgumentsPerToken = true;
-            downloadCommand.Add(parameterParam);
-            installCommand.Add(parameterParam);
-
-            var verifyOnlyParam = new Option<bool>("--verify", "Verify actions without making any changes");
-            verifyOnlyParam.SetDefaultValue(false);
-            verifyOnlyParam.IsRequired = false;
-            verifyOnlyParam.AddAlias("-v");
-            downloadCommand.Add(verifyOnlyParam);
-            installCommand.Add(verifyOnlyParam);
-
-            downloadCommand.SetHandler(async (registryConfig, downloaderConfig, components, parameters, verifyOnly) =>
-            {
-                await ExecuteDownloadCommandAsync(registryConfig, downloaderConfig, components, parameters, verifyOnly, args);
-            }, registryParam, downloadConfigParam, componentParam, parameterParam, verifyOnlyParam);
-
-            installCommand.SetHandler(async (registryConfig, installerConfig, components, parameters, verifyOnly) =>
-            {
-                await ExecuteInstallCommandAsync(registryConfig, installerConfig, components, parameters, verifyOnly, args);
-            }, registryParam, installConfigParam, componentParam, parameterParam, verifyOnlyParam);
+            var downloadCommand = SetupDownloadCommand(args);
+            var installCommand = SetupInstallCommand(args);
+            var chunkCommand = SetupChunkCommand(args);
+            var unchunkCommand = SetupUnchunkCommand(args);
+            var compressCommand = SetupCompressCommand(args);
+            var decompressCommand = SetupDecompressCommand(args);
 
             var root = new RootCommand("Installer for the Common Core platform");
             root.TreatUnmatchedTokensAsErrors = true;
             root.Add(downloadCommand);
             root.Add(installCommand);
+            root.Add(chunkCommand);
+            root.Add(unchunkCommand);
+            root.Add(compressCommand);
+            root.Add(decompressCommand);
 
             var result = await root.InvokeAsync(args);
             await Task.Delay(1000);
             return result;
         }
 
+        private static Command SetupDownloadCommand(string[] args)
+        {
+            var command = new Command("download", "Download components for installation");
+            command.TreatUnmatchedTokensAsErrors = true;
+
+            var registryParam = new Option<FileInfo>("--registry", "Location of registry configuration");
+            registryParam.IsRequired = true;
+            registryParam.AddAlias("-r");
+            command.Add(registryParam);
+
+            var downloadConfigParam = new Option<FileInfo>("--download", "Location of download configuration");
+            downloadConfigParam.IsRequired = false;
+            downloadConfigParam.AddAlias("-d");
+            command.Add(downloadConfigParam);
+
+            var componentParam = new Option<string[]>("--component", "Specific component to process");
+            componentParam.IsRequired = false;
+            componentParam.AddAlias("-c");
+            componentParam.AllowMultipleArgumentsPerToken = true;
+            command.Add(componentParam);
+
+            var parameterParam = new Option<string[]>("--parameter", "Specific colon separated key value pair to use as a config parameter");
+            parameterParam.IsRequired = false;
+            parameterParam.AddAlias("-p");
+            parameterParam.AllowMultipleArgumentsPerToken = true;
+            command.Add(parameterParam);
+
+            var verifyOnlyParam = new Option<bool>("--verify", "Verify actions without making any changes");
+            verifyOnlyParam.SetDefaultValue(false);
+            verifyOnlyParam.IsRequired = false;
+            verifyOnlyParam.AddAlias("-v");
+            command.Add(verifyOnlyParam);
+
+            command.SetHandler(async (registryConfig, downloaderConfig, components, parameters, verifyOnly) =>
+            {
+                await ExecuteDownloadCommandAsync(registryConfig, downloaderConfig, components, parameters, verifyOnly, args);
+            }, registryParam, downloadConfigParam, componentParam, parameterParam, verifyOnlyParam);
+
+            return command;
+        }
+
+        private static Command SetupInstallCommand(string[] args)
+        {
+            var command = new Command("install", "Install components");
+            command.TreatUnmatchedTokensAsErrors = true;
+
+            var registryParam = new Option<FileInfo>("--registry", "Location of registry configuration");
+            registryParam.IsRequired = true;
+            registryParam.AddAlias("-r");
+            command.Add(registryParam);
+
+            var installConfigParam = new Option<FileInfo>("--install", "Location of install configuration");
+            installConfigParam.IsRequired = false;
+            installConfigParam.AddAlias("-i");
+            command.Add(installConfigParam);
+
+            var componentParam = new Option<string[]>("--component", "Specific component to process");
+            componentParam.IsRequired = false;
+            componentParam.AddAlias("-c");
+            componentParam.AllowMultipleArgumentsPerToken = true;
+            command.Add(componentParam);
+
+            var parameterParam = new Option<string[]>("--parameter", "Specific colon separated key value pair to use as a config parameter");
+            parameterParam.IsRequired = false;
+            parameterParam.AddAlias("-p");
+            parameterParam.AllowMultipleArgumentsPerToken = true;
+            command.Add(parameterParam);
+
+            var verifyOnlyParam = new Option<bool>("--verify", "Verify actions without making any changes");
+            verifyOnlyParam.SetDefaultValue(false);
+            verifyOnlyParam.IsRequired = false;
+            verifyOnlyParam.AddAlias("-v");
+            command.Add(verifyOnlyParam);
+
+            command.SetHandler(async (registryConfig, installerConfig, components, parameters, verifyOnly) =>
+            {
+                await ExecuteInstallCommandAsync(registryConfig, installerConfig, components, parameters, verifyOnly, args);
+            }, registryParam, installConfigParam, componentParam, parameterParam, verifyOnlyParam);
+
+            return command;
+        }
+
+        private static Command SetupChunkCommand(string[] args)
+        {
+            var command = new Command("chunk", "Break a file into multiple smaller pieces");
+            command.TreatUnmatchedTokensAsErrors = true;
+
+            var sourceParam = new Option<FileInfo>("--source", "File to separate into chunks");
+            sourceParam.IsRequired = true;
+            sourceParam.AddAlias("-s");
+            command.Add(sourceParam);
+
+            var destParam = new Option<DirectoryInfo>("--dest", "Directory to store pieces in");
+            destParam.IsRequired = true;
+            destParam.AddAlias("-d");
+            command.Add(destParam);
+
+            var sizeParam = new Option<int>("--size", "Size in bytes to limit chunks to");
+            sizeParam.IsRequired = true;
+            sizeParam.AddAlias("-sz");
+            command.Add(sizeParam);
+
+            command.SetHandler(async (source, dest, size) =>
+            {
+                await ExecuteChunkCommandAsync(source, dest, size, args);
+            }, sourceParam, destParam, sizeParam);
+
+            return command;
+        }
+
+        private static Command SetupUnchunkCommand(string[] args)
+        {
+            var command = new Command("unchunk", "Merges file chunks into a single file");
+            command.TreatUnmatchedTokensAsErrors = true;
+
+            var sourceParam = new Option<DirectoryInfo>("--source", "Directory where file chunks are");
+            sourceParam.IsRequired = true;
+            sourceParam.AddAlias("-s");
+            command.Add(sourceParam);
+
+            var destParam = new Option<FileInfo>("--dest", "File to write combined pieces to");
+            destParam.IsRequired = true;
+            destParam.AddAlias("-d");
+            command.Add(destParam);
+
+            command.SetHandler(async (source, dest) =>
+            {
+                await ExecuteUnchunkCommandAsync(source, dest, args);
+            }, sourceParam, destParam);
+
+            return command;
+        }
+
+        private static Command SetupCompressCommand(string[] args)
+        {
+            var command = new Command("compress", "Compresses a directory into a single file");
+            command.TreatUnmatchedTokensAsErrors = true;
+
+            var sourceParam = new Option<DirectoryInfo>("--source", "Directory to compress");
+            sourceParam.IsRequired = true;
+            sourceParam.AddAlias("-s");
+            command.Add(sourceParam);
+
+            var destParam = new Option<FileInfo>("--dest", "File to write compressed contents to");
+            destParam.IsRequired = true;
+            destParam.AddAlias("-d");
+            command.Add(destParam);
+
+            command.SetHandler(async (source, dest) =>
+            {
+                await ExecuteCompressCommandAsync(source, dest, args);
+            }, sourceParam, destParam);
+
+            return command;
+        }
+
+        private static Command SetupDecompressCommand(string[] args)
+        {
+            var command = new Command("decompress", "Decompress a file back into directory structure");
+            command.TreatUnmatchedTokensAsErrors = true;
+
+            var sourceParam = new Option<FileInfo>("--source", "File to decompress");
+            sourceParam.IsRequired = true;
+            sourceParam.AddAlias("-s");
+            command.Add(sourceParam);
+
+            var destParam = new Option<DirectoryInfo>("--dest", "Directory write contents to");
+            destParam.IsRequired = true;
+            destParam.AddAlias("-d");
+            command.Add(destParam);
+
+            command.SetHandler(async (source, dest) =>
+            {
+                await ExecuteDecompressCommandAsync(source, dest, args);
+            }, sourceParam, destParam);
+
+            return command;
+        }
+
         private static async Task ExecuteDownloadCommandAsync(FileInfo registryConfig, FileInfo downloaderConfig, string[] components, string[] parameters, bool verifyOnly, string[] args)
         {
-            var builder = Host.CreateApplicationBuilder(args);
-            var (_, loggerFactory) = ConfigureLogging(builder.Logging);
-
+            var (_, loggerFactory) = Initialize(args);
             var configParameters = BuildConfigParameters(parameters);
 
             var dataRequest = new DataRequestService(loggerFactory, verifyOnly);
@@ -93,14 +229,51 @@ namespace Abs.CommonCore.Installer
 
         private static async Task ExecuteInstallCommandAsync(FileInfo registryConfig, FileInfo installerConfig, string[] components, string[] parameters, bool verifyOnly, string[] args)
         {
-            var builder = Host.CreateApplicationBuilder(args);
-            var (_, loggerFactory) = ConfigureLogging(builder.Logging);
-
+            var (_, loggerFactory) = Initialize(args);
             var configParameters = BuildConfigParameters(parameters);
 
             var commandExecution = new CommandExecutionService(loggerFactory, verifyOnly);
             var installer = new ComponentInstaller(loggerFactory, commandExecution, registryConfig, installerConfig, configParameters);
             await installer.ExecuteAsync(components);
+        }
+
+        private static async Task ExecuteChunkCommandAsync(FileInfo source, DirectoryInfo destination, int size, string[] args)
+        {
+            var (_, loggerFactory) = Initialize(args);
+
+            var chunker = new ChunkerService(loggerFactory);
+            await chunker.ChunkFile(source, destination, size);
+        }
+
+        private static async Task ExecuteUnchunkCommandAsync(DirectoryInfo source, FileInfo destination, string[] args)
+        {
+            var (_, loggerFactory) = Initialize(args);
+
+            var chunker = new ChunkerService(loggerFactory);
+            await chunker.UnchunkFile(source, destination);
+        }
+
+        private static async Task ExecuteCompressCommandAsync(DirectoryInfo source, FileInfo destination, string[] args)
+        {
+            var (_, loggerFactory) = Initialize(args);
+
+            var chunker = new CompressionService(loggerFactory);
+            await chunker.CompressDirectory(source, destination);
+        }
+
+
+        private static async Task ExecuteDecompressCommandAsync(FileInfo source, DirectoryInfo destination, string[] args)
+        {
+            var (_, loggerFactory) = Initialize(args);
+
+            var chunker = new CompressionService(loggerFactory);
+            await chunker.DecompressFile(source, destination);
+        }
+
+        private static (ILogger, ILoggerFactory) Initialize(string[] args)
+        {
+            var builder = Host.CreateApplicationBuilder(args);
+            return ConfigureLogging(builder.Logging);
         }
 
         private static (ILogger, ILoggerFactory) ConfigureLogging(ILoggingBuilder builder)
