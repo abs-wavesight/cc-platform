bc5a30e7ec0fe7e24e4b742243f86b99c7b4fcdf
Patrick Morris <pmorris@eagle.org>
2023-07-21 13:37:48 -0700

    Added tests for extensions

---
+++ libraries/Platform/CommonCore.Platform.Tests/Extensions/ForAllAsyncTests.cs
@@ -0,0 +1,28 @@
+﻿using Abs.CommonCore.Platform.Extensions;
+using Xunit;
+
+namespace Abs.CommonCore.Platform.Tests.Extensions
+{
+    public class ForAllAsyncExtensionTests
+    {
+        [Fact]
+        public async Task TestForAllAsync()
+        {
+            var items = Enumerable.Range(0, 100)
+                .Select(i => DateTime.Now.Ticks % 1000)
+                .ToArray();
+
+            var concurrentSum = 0L;
+            await items
+                .ForAllAsync(async x =>
+                {
+                    await Task.Yield(); // Force concurrency
+                    Interlocked.Add(ref concurrentSum, x);
+                });
+
+            var expectedSum = items.Sum();
+
+            Assert.Equal(expectedSum, concurrentSum);
+        }
+    }
+}
