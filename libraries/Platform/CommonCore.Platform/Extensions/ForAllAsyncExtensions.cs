538e1fede55313e3f969f4c3ab5deac4b88b69e9
Patrick Morris <pmorris@eagle.org>
2023-07-21 13:16:02 -0700

    Added helpful extension methods

---
+++ libraries/Platform/CommonCore.Platform/Extensions/ForAllAsyncExtensions.cs
@@ -0,0 +1,36 @@
+﻿namespace Abs.CommonCore.Platform.Extensions
+{
+    public static class ForAllAsyncExtensions
+    {
+        public static Task ForAllAsync<T>(
+            this IEnumerable<T> collection,
+            Func<T, Task> asyncItemAction,
+            int maxDegreesOfParallelism = 0,
+            CancellationToken cancellationToken = default)
+        {
+            collection = collection ?? throw new ArgumentNullException(nameof(collection));
+            asyncItemAction = asyncItemAction ?? throw new ArgumentNullException(nameof(asyncItemAction));
+
+            if (maxDegreesOfParallelism <= 0)
+            {
+                return Parallel.ForEachAsync(collection, cancellationToken, (i, c) => ProcessItemAsync(i, c, asyncItemAction));
+            }
+
+            var options = new ParallelOptions
+            {
+                MaxDegreeOfParallelism = maxDegreesOfParallelism,
+                CancellationToken = cancellationToken
+            };
+
+            return Parallel.ForEachAsync(collection, options, (i, c) => ProcessItemAsync(i, c, asyncItemAction));
+        }
+
+        private static ValueTask ProcessItemAsync<T>(T item, CancellationToken cancellation, Func<T, Task> asyncItemAction)
+        {
+            cancellation.ThrowIfCancellationRequested();
+
+            var task = asyncItemAction(item);
+            return new ValueTask(task);
+        }
+    }
+}
---
+++ libraries/Platform/CommonCore.Platform/Extensions/TaskExtensions.cs
@@ -0,0 +1,29 @@
+﻿namespace Abs.CommonCore.Platform.Extensions
+{
+    public static class TaskExtensions
+    {
+        #region Public Methods
+
+        public static Task WhenAllAsync(this IEnumerable<Task> tasks)
+        {
+            return Task.WhenAll(tasks);
+        }
+
+        public static Task<TResult[]> WhenAllAsync<TResult>(this IEnumerable<Task<TResult>> tasks)
+        {
+            return Task.WhenAll(tasks);
+        }
+
+        public static async Task WhenAnyAsync(this IEnumerable<Task> tasks)
+        {
+            await await Task.WhenAny(tasks);
+        }
+
+        public static async Task<TResult> WhenAnyAsync<TResult>(this IEnumerable<Task<TResult>> tasks)
+        {
+            return await await Task.WhenAny(tasks);
+        }
+
+        #endregion Public Methods
+    }
+}
