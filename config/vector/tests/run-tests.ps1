880682b73deb798c0138ed63b5214a9a87543b83
Ryan Henderson <130497203+ryanhendersonabs@users.noreply.github.com>
2023-07-26 08:24:30 -0500

    Feature/ADO-21033: Central vector log consumption (#33)
    * Fixed vector launch issue; added "central" and "site" flavors of vector
    
    * Commenting out tcp docker host
    
    * Fixed vector loki variant config
    
    * Fixed up central log mapping
    
    * Fixed tests
    
    * Addressed todos
    
    * Fixed existing vector tests
    
    * Cleaned up ps logging
    
    * Added first few central vector tests
    
    * Added more tests; debugging test failure reporting in CI
    
    * Debugging test failure reporting in CI
    
    * Fixed tests
    
    * Commented out docker host
    
    * Removed unnecessary parse step

--- config/vector/tests/run-tests.ps1
+++ config/vector/tests/run-tests.ps1
@@ -1,15 +1,34 @@
 $ScriptDirectory = $(Split-Path $MyInvocation.MyCommand.Path)
 $RepoDirectory = (Get-Item $ScriptDirectory).Parent.Parent.Parent.FullName
-$LocalConfigDirectory = "${RepoDirectory}/config/vector/config"
 
-$Image = "ghcr.io/abs-wavesight/vector:windows-2019"
+$Image = "vector-local-for-tests:windows-2019"
 
-$ContainerConfigFilePath = "C:\\config\\vector.toml"
+# First, build a local docker image so we're testing the latest version of the image, rather than the one from the registry
+$BuildCommand = "docker build -f $RepoDirectory/containers/vector/Dockerfile -t $Image --build-arg SERVER_VERSION=ltsc2019 --build-arg DOTNET_TAG=7.0-nanoserver-1809 --build-arg POWERSHELL_NANOSERVER_TAG=lts-7.2-nanoserver-1809 --platform windows/amd64 containers/vector"
+Write-Output "Docker Build Command: ${BuildCommand}"
+Invoke-Expression $BuildCommand
 
-$TestFileDirectory = "${RepoDirectory}/config/vector/tests"
-$TestFiles = Get-ChildItem $TestFileDirectory -Filter *.toml | ForEach-Object {"C:\\tests\\" + $_.Name}
-$TestFilesParameter = $TestFiles -join " "
+$TestExitCode = 0
+$VectorVariants = @("central", "site")
+foreach($VectorVariant in $VectorVariants)
+{
+  $LocalConfigDirectory = "${RepoDirectory}/config/vector/config/$VectorVariant"
+  $ContainerConfigFilePath = "C:/config/vector.toml"
+  $TestFileDirectory = "${RepoDirectory}/config/vector/tests/$VectorVariant"
+  $TestFiles = Get-ChildItem $TestFileDirectory -Filter *.toml | ForEach-Object {"C:/tests/" + $_.Name}
+  $ConfigFilesParameter = $TestFiles -join ","
+  $ConfigFilesParameter = $ConfigFilesParameter += ",$ContainerConfigFilePath"
 
-$Command = "docker run --mount type=bind,src=${LocalConfigDirectory},dst=C:/config --mount type=bind,src=${TestFileDirectory},dst=C:/tests ${Image} test ${ContainerConfigFilePath} ${TestFilesParameter}"
-Write-Output "Docker Command: ${Command}"
-Invoke-Expression $Command
+  $RunCommand = "docker run --mount type=bind,src=${LocalConfigDirectory},dst=C:/config --mount type=bind,src=${TestFileDirectory},dst=C:/tests -e VECTOR_LOG_FORMAT=json -e VECTOR_COLOR=never -e VECTOR_CONFIG=$ConfigFilesParameter -e VECTOR_TEST=true ${Image} test"
+  Write-Output "Docker Run Command: ${RunCommand}"
+  Invoke-Expression $RunCommand
+
+  # Capture error codes so the step will fail in CI
+  Write-Output "LastExitCode from $VectorVariant tests: $LastExitCode"
+  if ($LastExitCode -ne 0) {
+    $TestExitCode = $LastExitCode
+  }
+}
+
+Write-Output "Exiting with code: $TestExitCode"
+exit $TestExitCode
--- containers/vector/scripts/startup.ps1
+++ containers/vector/scripts/startup.ps1
@@ -9,4 +9,15 @@ Write-Output "Running script to set up log retention scheduled task..."
 & "C:\\scripts\\create-scheduled-task.ps1" -LogsDir "C:\\cc-logs" -Before $LogRetentionPeriod
 
 # Run Vector
-& "C:\\Program Files\\Vector\\bin\\vector.exe"
+if ($Env:VECTOR_TEST -eq "true")
+{
+  Write-Output "Running Vector tests..."
+  & "C:\\Program Files\\Vector\\bin\\vector.exe" test ${Env:VECTOR_CONFIG}
+  Write-Output "LastExitCode: $LastExitCode"
+  exit $LastExitCode
+}
+else
+{
+  Write-Output "Running Vector..."
+  & "C:\\Program Files\\Vector\\bin\\vector.exe"
+}
