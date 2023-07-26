c2ffe8171c23910f89d2d40b58edc4aa513be5f9
DmytroHurov <135887254+DmytroHurov@users.noreply.github.com>
2023-07-25 21:47:06 -0500

    Added rabbit configurer (#32)
    * Added rabbit configurer
    
    * Fixed comments
    
    * Fixed WHITESPACE
    
    * Fixed minor comments
    
    * Removed EasyNetQ from interface
    
    * Universalize for RMQ and Azure

---
+++ libraries/Platform/CommonCore.Platform/MessengerConfigurator/IMessengerConfigurator.cs
@@ -0,0 +1,10 @@
+﻿namespace Abs.CommonCore.Platform.MessengerConfigurator
+{
+    public interface IMessengerConfigurator
+    {
+        Task CreateDistributorAsync(string busKey, MessageDistributor distributor, CancellationToken cancellationToken = default);
+        Task CreateDistributorsWithDeliveriesAsync(string busKey, List<MessageDistributor> distributors, CancellationToken cancellationToken = default);
+        Task CreateDeliverymanAsync(string busKey, MessageDeliveryman deliveryman, string vhost, CancellationToken cancellationToken = default);
+        Task BindDistributorAndDeliverymenAsync(string busKey, string distributorName, string deliverymanName, string bindingName = "", string vhostName = "/", CancellationToken cancellationToken = default);
+    }
+}
---
+++ libraries/Platform/CommonCore.Platform/MessengerConfigurator/RabbitConfigurerExchange.cs
@@ -0,0 +1,16 @@
+﻿using EasyNetQ.Management.Client.Model;
+
+namespace Abs.CommonCore.Platform.MessengerConfigurator
+{
+    public class RabbitConfigurerExchange
+    {
+        public RabbitConfigurerExchange(string name, string type)
+        {
+            Info = new ExchangeInfo(name, type);
+        }
+
+        public ExchangeInfo Info { get; private set; }
+        public string Vhost { get; set; } = "/";
+        public List<QueueInfo> Queues { get; set; } = new List<QueueInfo>();
+    }
+}
