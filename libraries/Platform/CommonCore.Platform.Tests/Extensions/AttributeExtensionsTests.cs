dc63771ef42f08254dc8d9b886b85de1566beb49
Yevhenii Lysianyi <140668615+YLysianyi@users.noreply.github.com>
2023-08-21 10:17:58 -0500

    Feature/ado 21973 enum description (#98)
    * Extension method for enum to get description
    
    * Move GetDescription expension to CC.Platform

--- libraries/Platform/CommonCore.Platform.Tests/Extensions/AttributeExtensionsTests.cs
+++ libraries/Platform/CommonCore.Platform.Tests/Extensions/AttributeExtensionsTests.cs
@@ -3,46 +3,69 @@ using Abs.CommonCore.Platform.Extensions;
 using FluentAssertions;
 using Xunit;
 
-namespace Abs.CommonCore.Platform.Tests.Extensions
+namespace Abs.CommonCore.Platform.Tests.Extensions;
+
+public class AttributeExtensionsTests
 {
-    public class AttributeExtensionsTests
+    private const string DescriptionPrefix = "EnumDescription_";
+
+    [Theory]
+    [InlineData(TestEnum.A, $"{DescriptionPrefix}A")]
+    [InlineData(TestEnum.B, $"{DescriptionPrefix}B")]
+    [InlineData(TestEnum.X, null)]
+    public void GetDescription_TestEnumValue_CorrectDescription(TestEnum input, string? expectedResult)
     {
-        [Fact]
-        public void GetDescription_GivenDescriptionAttribute_ShouldReturnDescription()
-        {
-            // Act
-            var result = typeof(DescriptionTestDummyClass).GetDescription(nameof(DescriptionTestDummyClass.PropOne));
+        // Act
+        var result = input.GetDescription();
 
-            // Assert
-            result.Should().Be("prop one");
-        }
+        // Assert
+        Assert.Equal(expectedResult, result);
+    }
 
-        [Fact]
-        public void GetDescription_GivenNoDescriptionAttribute_ShouldReturnNull()
-        {
-            // Act
-            var result = typeof(DescriptionTestDummyClass).GetDescription(nameof(DescriptionTestDummyClass.PropTwo));
+    [Fact]
+    public void GetDescription_GivenDescriptionAttribute_ShouldReturnDescription()
+    {
+        // Act
+        var result = typeof(DescriptionTestDummyClass).GetDescription(nameof(DescriptionTestDummyClass.PropOne));
 
-            // Assert
-            result.Should().BeNull();
-        }
+        // Assert
+        result.Should().Be("prop one");
+    }
 
-        [Fact]
-        public void GetDescription_GivenInvalidProperty_ShouldReturnNull()
-        {
-            // Act
-            var result = typeof(DescriptionTestDummyClass).GetDescription("not a prop");
+    [Fact]
+    public void GetDescription_GivenNoDescriptionAttribute_ShouldReturnNull()
+    {
+        // Act
+        var result = typeof(DescriptionTestDummyClass).GetDescription(nameof(DescriptionTestDummyClass.PropTwo));
 
-            // Assert
-            result.Should().BeNull();
-        }
+        // Assert
+        result.Should().BeNull();
+    }
 
-        public class DescriptionTestDummyClass
-        {
-            [Description("prop one")]
-            public string? PropOne { get; set; }
+    [Fact]
+    public void GetDescription_GivenInvalidProperty_ShouldReturnNull()
+    {
+        // Act
+        var result = typeof(DescriptionTestDummyClass).GetDescription("not a prop");
 
-            public string? PropTwo { get; set; }
-        }
+        // Assert
+        result.Should().BeNull();
+    }
+
+    public class DescriptionTestDummyClass
+    {
+        [Description("prop one")]
+        public string? PropOne { get; set; }
+
+        public string? PropTwo { get; set; }
+    }
+
+    public enum TestEnum
+    {
+        [Description($"{DescriptionPrefix}A")]
+        A,
+        [Description($"{DescriptionPrefix}B")]
+        B,
+        X
     }
 }
