869efc7076233eab29cafde58511048c02641486
Patrick Morris <pmorris@eagle.org>
2023-08-23 10:52:10 -0700

    ADO-22076: Fixed broken dev utility test (#101)
    * Fixed broken test

--- tools/LocalDevUtility/LocalDevUtility/Helpers/DockerHelper.cs
+++ tools/LocalDevUtility/LocalDevUtility/Helpers/DockerHelper.cs
@@ -13,9 +13,9 @@ public static class DockerHelper
         using (CliStep.Start("Resetting Docker", true))
         {
             powerShellAdapter.RunPowerShellCommand("docker ps -aq | ForEach-Object { Write-Output \"Stopping $(docker stop $_) & removing $(docker rm $_)\" };");
-            powerShellAdapter.RunPowerShellCommand("Write-Output \"Pruning system: $(docker system prune -f)\"");
-            powerShellAdapter.RunPowerShellCommand("Write-Output \"Pruning volume: $(docker volume prune -f)\"");
-            powerShellAdapter.RunPowerShellCommand("Write-Output \"Pruning network: $(docker network prune -f)\"");
+            powerShellAdapter.RunPowerShellCommand("Write-Output \"Pruning system: $(docker system prune -f)\" ");
+            powerShellAdapter.RunPowerShellCommand("Write-Output \"Pruning volume: $(docker volume prune -f)\" ");
+            powerShellAdapter.RunPowerShellCommand("Write-Output \"Pruning network: $(docker network prune -f)\" ");
         }
     }
 
