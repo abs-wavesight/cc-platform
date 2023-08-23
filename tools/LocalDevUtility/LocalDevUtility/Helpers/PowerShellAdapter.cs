e21c696521390e5578699849ef9341fb5a54b1c0
Patrick Morris <pmorris@eagle.org>
2023-08-23 01:16:56 -0700

    Commented out timeout

--- tools/LocalDevUtility/LocalDevUtility/Helpers/PowerShellAdapter.cs
+++ tools/LocalDevUtility/LocalDevUtility/Helpers/PowerShellAdapter.cs
@@ -27,11 +27,11 @@ public class PowerShellAdapter : IPowerShellAdapter
         ps.Streams.Error.DataAdded += (sender, args) => ProcessCommandOutput(rawOutput, logger, sender, args);
 
         var task = ps.InvokeAsync<object, string>(null, output);
-        if (timeout is not null)
-        {
-            task = task
-                .WaitAsync(timeout.Value);
-        }
+        //if (timeout is not null)
+        //{
+        //    task = task
+        //        .WaitAsync(timeout.Value);
+        //}
 
         task
             .GetAwaiter()
