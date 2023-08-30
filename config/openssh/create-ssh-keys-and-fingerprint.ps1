d46296879aaa49edc26bc001fceb8b585196704e
Yevhenii Lysianyi <140668615+YLysianyi@users.noreply.github.com>
2023-08-30 15:43:34 -0500

    Update ssh key generation (#119)
    * Update ssh key generation
    
    * Rename key folder
    
    * Update LocalDevUntility with ssh-keys path
    
    * Add open ssh client instal option
    
    * Rename ssh key path

---
+++ config/openssh/create-ssh-keys-and-fingerprint.ps1
@@ -0,0 +1,32 @@
+﻿param ($output_path)
+
+New-Item -Type Directory $output_path -Force > $null
+$ssh_host_rsa_key_path = @(Join-Path -Path $output_path -ChildPath "ssh_host_rsa_key")
+# Generate ssh host rsa key
+if (Test-Path -Path $ssh_host_rsa_key_path)
+{
+    Write-Output "$ssh_host_rsa_key_path already exists."
+}
+else 
+{
+    @(ssh-keygen -f $ssh_host_rsa_key_path -q -N '\\\"\\\"')
+}
+
+# Generate fingerprint
+if (Test-Path -Path $ssh_host_rsa_key_path)
+{
+    # Default: 3072 SHA256:Wl1/wCywwvUOrs8J2++YnvKgGmiokFgDc9J8fu8Dwl0 eagle\user@APQ1mA07ItRkr3t (RSA)
+    # Expected: ssh-rsa 3072 Wl1/wCywwvUOrs8J2++YnvKgGmiokFgDc9J8fu8Dwl0
+    $fingerprint = @(ssh-keygen -lf $ssh_host_rsa_key_path)
+    $temp = $fingerprint.split(" ");
+    $len = $temp[0] # 3072
+    $value = $temp[1].split(":")[1] # Wl1/wCywwvUOrs8J2++YnvKgGmiokFgDc9J8fu8Dwl0
+
+    $fingerprint_file = @(Join-Path -Path $output_path -ChildPath "SshHostKeyFingerprint")
+    "ssh-rsa $len $value" | Out-File "$fingerprint_file"
+}
+else
+{
+    Write-Output "$ssh_host_rsa_key_path not found."
+    exit 1
+}
