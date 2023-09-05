96515420fd631c538577b6829120adc5550ad33a
Yevhenii Lysianyi <YLysianyi@eagle.org>
2023-09-05 18:38:25 -0500

    Update create-dre-user logic

--- containers/openssh/scripts/startup.ps1
+++ containers/openssh/scripts/startup.ps1
@@ -1,4 +1,4 @@
-try
+﻿try
 {
   $SshConfigFilePath = "C:/ProgramData/ssh/sshd_config"
   $SshTempConfigFilePath = "C:/ProgramData/ssh/sshd.config"
@@ -14,32 +14,25 @@ try
   Write-Output "`nCopying OpenSSH config files..."
   Copy-Item "C:/config/sshd.config" -Destination "C:/ProgramData/ssh" -Force
 
+  $drexGroupName = "drex_group"
   Write-Output "`nCreating DREX local user group..."
-  & net localgroup "drex_group" /ADD
+  & net localgroup $drexGroupName /ADD
 
   $Config = Get-Content -Path "C:/config/config.json" -Raw | ConvertFrom-Json
   foreach($ClientName in $Config.clients)
   {
-    Write-Output "`nCreating user (if not exists) for client $ClientName..."
-    & net USER $ClientName $Config.defaultPassword /ADD && net localgroup "Administrators" $ClientName /ADD
+    $username = $ClientName
+    $password = $Config.defaultPassword
+    & C:\\scripts\\create-drex-user.ps1 $username $password $false $false
+  }
 
-    Write-Output "`nCreating directory for client $ClientName..."
-    New-Item -Path "C:/sftproot" -Name $ClientName -ItemType "directory" -Force
+  Write-Output "`nAdding users..."
+  foreach($User in $Config.users)
+  {
+    $username = $User.username
+    $password = $User.password
 
-    
-    Write-Output "`Adding match user block for client $ClientName..."
-    $MatchUserBlock = @"
-
-Match User ${ClientName}
-  ChrootDirectory c:\sftproot\${ClientName}\
-  PermitTunnel no
-  AllowAgentForwarding no
-  AllowTcpForwarding no
-  X11Forwarding no
-  GatewayPorts no
-
-"@
-    Add-Content $SshTempConfigFilePath $MatchUserBlock
+    & C:\\scripts\\create-drex-user.ps1 $username $password $true $false
   }
 
   Write-Output "`nOpenSSH Config file contents:"
---
+++ tools/Installer/Installer/OptionValidators/AddOpenSshUserCommandOptionsValidator.cs
@@ -0,0 +1,26 @@
+﻿using System.CommandLine.Parsing;
+using System.Text.RegularExpressions;
+
+namespace Abs.CommonCore.Installer.OptionValidators;
+public partial class AddOpenSshUserCommandOptionsValidator
+{
+    [GeneratedRegex("^[a-zA-Z][a-zA-Z0-9]{4,11}$", RegexOptions.IgnoreCase, "en-US")]
+    private static partial Regex UsernameRegex();
+
+    public static void ValidateUserName(OptionResult symbolResult)
+    {
+        var username = symbolResult.Tokens.FirstOrDefault()?.Value;
+        if (username == null)
+        {
+            var errorMessage = $"{symbolResult.Option.Name} is required.";
+            symbolResult.ErrorMessage = errorMessage;
+            return;
+        }
+
+        if (!UsernameRegex().IsMatch(username))
+        {
+            var errorMessage = $"'{username}' is not valid.";
+            symbolResult.ErrorMessage = errorMessage;
+        }
+    }
+}
