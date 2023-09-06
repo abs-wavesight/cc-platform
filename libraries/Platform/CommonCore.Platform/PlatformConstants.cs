80b90e3a536114fc3f0dbdc846d0b6192b8a7cd6
Yevhenii Lysianyi <YLysianyi@eagle.org>
2023-09-06 14:58:05 -0500

    Update docker compose files

--- libraries/Platform/CommonCore.Platform/PlatformConstants.cs
+++ libraries/Platform/CommonCore.Platform/PlatformConstants.cs
@@ -1,9 +1,10 @@
-﻿namespace Abs.CommonCore.Platform
+﻿namespace Abs.CommonCore.Platform;
+
+public static class PlatformConstants
 {
-    public static class PlatformConstants
-    {
-        public const string SSH_Keys_Path = "ABS_CC_SSH_KEYS_PATH";
-        public const string FDZ_Path = "ABS_CC_FDZ_PATH";
-        public const string SFTP_Path = "ABS_CC_SFTP_PATH";
-    }
+    public static readonly string SSH_Keys_Path = "ABS_CC_SSH_KEYS_PATH";
+    public static readonly string FDZ_Path = "ABS_CC_FDZ_PATH";
+    public static readonly string SFTP_Path = "ABS_CC_SFTP_PATH";
+
+    public static readonly string FlatLogsVariableName = "FLAT_LOGS";
 }
