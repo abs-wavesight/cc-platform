2e4c2c71815ed7947491678a98ab5d5d0d02e649
Yevhenii Lysianyi <140668615+YLysianyi@users.noreply.github.com>
2023-09-06 11:44:42 -0500

    Add logging configuration (#135)
    * Add logging configuration
    
    * Fix formatting
    
    * Fix formatting

---
+++ libraries/Platform/CommonCore.Platform/Extensions/ServiceRegistrar.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+
+namespace Abs.CommonCore.Platform.Extensions;
+
+[ExcludeFromCodeCoverage]
+public static class ServiceRegistrar
+{
+    private const string FlatLogsVariableName = "FLAT_LOGS";
+    public static ILoggingBuilder ConfigureLogging(this ILoggingBuilder logging, bool? useFlatLogs = null)
+    {
+        logging.ClearProviders();
+
+        useFlatLogs ??= ShouldUseFlatLogs();
+        if (useFlatLogs.Value)
+        {
+            logging.AddSimpleConsole(options =>
+            {
+                options.IncludeScopes = false;
+                options.SingleLine = true;
+                options.TimestampFormat = "yyyy-MM-dd HH:mm:ss:ffffff ";
+                options.ColorBehavior = LoggerColorBehavior.Enabled;
+            });
+        }
+        else
+        {
+            logging.AddJsonConsole(options =>
+            {
+                options.TimestampFormat = "u";
+                options.IncludeScopes = true;
+                options.JsonWriterOptions = new JsonWriterOptions
+                {
+                    Indented = false,
+                    Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+                };
+            });
+        }
+
+        return logging;
+    }
+
+    private static bool ShouldUseFlatLogs()
+    {
+        var flatLogsStr = Environment.GetEnvironmentVariable(FlatLogsVariableName);
+        if (!bool.TryParse(flatLogsStr, out var useFlatLogs))
+        {
+            useFlatLogs = false;
+        }
+
+        return useFlatLogs;
+    }
+}
