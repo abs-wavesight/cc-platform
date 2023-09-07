5354ec6911f9c9ba434d72f4c15fea773c2495a0
Yevhenii Lysianyi <YLysianyi@eagle.org>
2023-09-07 09:13:21 -0500

    Update consts

--- libraries/Platform/CommonCore.Platform/PlatformConstants.cs
+++ libraries/Platform/CommonCore.Platform/PlatformConstants.cs
@@ -2,9 +2,9 @@
 
 public static class PlatformConstants
 {
-    public static readonly string SSH_Keys_Path = "ABS_CC_SSH_KEYS_PATH";
-    public static readonly string FDZ_Path = "ABS_CC_FDZ_PATH";
-    public static readonly string SFTP_Path = "ABS_CC_SFTP_PATH";
+    public const string SSH_Keys_Path = "ABS_CC_SSH_KEYS_PATH";
+    public const string FDZ_Path = "ABS_CC_FDZ_PATH";
+    public const string SFTP_Path = "ABS_CC_SFTP_PATH";
 
-    public static readonly string FlatLogsVariableName = "FLAT_LOGS";
+    public const string FlatLogsVariableName = "FLAT_LOGS";
 }
