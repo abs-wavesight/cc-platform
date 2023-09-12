46713d86a6f2f9fdaa406a6c2c4370f471d66d5c
Yevhenii Lysianyi <YLysianyi@eagle.org>
2023-09-12 17:08:02 -0500

    Update create-drex-user.ps1

--- containers/openssh/scripts/create-drex-user.ps1
+++ containers/openssh/scripts/create-drex-user.ps1
@@ -28,14 +28,14 @@ if ($DrexUser)
   {
     Write-Output "Adding user to the config file.."
 
-    $user = $config.users | Where-Object { $_.username -eq "$Username" }
+    $user = $config.sites | Where-Object { $_.username -eq "$Username" }
     if ($null -ne $user)
     {
-        $config.users | % {if($_.username -eq "$Username"){$_.password="ads1111"}}
+        $config.sites | % {if($_.username -eq "$Username"){$_.password="$Password"}}
     }
     else
     {
-        $config.users += @{"username"="$Username";"password"="$Password"}
+        $config.sites += @{"username"="$Username";"password"="$Password"}
     }
 
     $config | ConvertTo-Json | set-content $configFile
@@ -43,7 +43,7 @@ if ($DrexUser)
 }
 else
 {
-  & net USER $Username $Password /ADD && net localgroup "Administrators" $Username /ADD
+  & net USER $Username $Password /ADD
 
   Write-Output "`nCreating directory for client $Username..."
   New-Item -Path "C:/sftproot" -Name $Username -ItemType "directory" -Force
