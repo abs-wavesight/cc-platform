8da4f278a60cdf21ad6e230321cdb985ec3c271a
Patrick Morris <pmorris@eagle.org>
2023-10-13 12:16:55 -0700

    Fixed tests

--- tools/Installer/Installer.Tests/Actions/ReleaseVersionProviderTests.cs
+++ tools/Installer/Installer.Tests/Actions/ReleaseVersionProviderTests.cs
@@ -7,9 +7,9 @@ namespace Installer.Tests.Actions;
 public class ReleaseVersionProviderTests : TestsBase
 {
     [Theory]
-    [InlineData("RabbitMQ Release 2019", new[] { "RabbitMQ Release 2022 v100.0.11", "RabbitMQ Release 2019 v100.0.10" }, "v100.0.10")]
-    [InlineData("RabbitMQ Release 2022", new[] { "RabbitMQ Release 2022 v100.0.11", "RabbitMQ Release 2019 v100.0.10" }, "v100.0.11")]
-    [InlineData("RabbitMQ Release 2025", new[] { "RabbitMQ Release 2022 v100.0.11", "RabbitMQ Release 2019 v100.0.10" }, null)]
+    [InlineData("RabbitMQ Release", new[] { "v100.0.11 Vector Release", "v100.0.10 RabbitMQ Release" }, "v100.0.10")]
+    [InlineData("RabbitMQ Release", new[] { "v100.0.11 RabbitMQ Release", "v100.0.10 RabbitMQ Release" }, "v100.0.11")]
+    [InlineData("Vector Release", new[] { "v100.0.11 RabbitMQ Release", "v100.0.10 RabbitMQ Release" }, null)]
     public async Task GetVersionAsync_GivenCollection_ReturnsExpected(string releaseName, IEnumerable<string> releaseNames, string? expectedResult)
     {
         // Arrange
