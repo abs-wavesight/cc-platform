e01146e855d18d428d1aa8d253d81aa55ddf5614
DHurov <DHurov@eagle.org>
2023-10-23 12:02:34 -0500

    Changed logging style

--- tools/Installer/Installer/Actions/ComponentDownloader.cs
+++ tools/Installer/Installer/Actions/ComponentDownloader.cs
@@ -55,18 +55,11 @@ public class ComponentDownloader : ActionBase
         _logger.LogInformation("Starting downloader");
         Directory.CreateDirectory(_registryConfig.Location);
 
-        var registredComponents = _registryConfig.Components.Select(c => c.Name).ToArray();
-        if (registredComponents != null && registredComponents.Length > 0)
-        {
-            var registerdComponentsLog = "Registred components: " + string.Join(Environment.NewLine + "----", registredComponents);
-            _logger.LogInformation(registerdComponentsLog);
-        }
+        var registerdComponentsLog = $"Required components: {_registryConfig.Components.Select(c => c.Name).ToArray().StringJoin(", ")}";
+        _logger.LogInformation(registerdComponentsLog);
 
-        if (specificComponents != null && specificComponents.Length > 0)
-        {
-            var requiredComponentsLog = "Required components: " + string.Join(Environment.NewLine + "----", specificComponents);
-            _logger.LogInformation(requiredComponentsLog);
-        }
+        var requiredComponentsLog = $"Required components: {specificComponents?.StringJoin(", ")}";
+        _logger.LogInformation(requiredComponentsLog);
 
         var components = DetermineComponents(_registryConfig, specificComponents, _downloaderConfig?.Components);
 
