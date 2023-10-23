cc33a8f33cdfdbfab4d905a9697b7891d4575572
Yevhenii Lysianyi <YLysianyi@eagle.org>
2023-10-23 17:21:56 -0500

    Update POC to use Google.Protobuf

--- POCs/ProtoJsonInput/ProtoSender/Program.cs
+++ POCs/ProtoJsonInput/ProtoSender/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System.Net.Http.Headers;
-using ProtoBuf;
 using System.Text;
-using System.Text.Json;
+using Google.Protobuf;
 using Models;
+using Newtonsoft.Json.Schema.Generation;
+using Newtonsoft.Json.Serialization;
 
 var model = new Model
 {
@@ -10,17 +11,17 @@ var model = new Model
     Data = "Some random Data"
 };
 
-byte[] protobufData;
-using (var stream = new MemoryStream())
-{
-    Serializer.Serialize(stream, model);
-    protobufData = stream.ToArray();
-}
+// To generate Json schema
+//var generator = new JSchemaGenerator { ContractResolver = new CamelCasePropertyNamesContractResolver() };
+//var schema = generator.Generate(typeof(Model));
+
+byte[] protobufData = model.ToByteArray();
 var protobufContent = new ByteArrayContent(protobufData);
 protobufContent.Headers.Add("Content-Type", "application/protobuf");
 await PostRequest(protobufContent, "application/protobuf");
 
-var json = JsonSerializer.Serialize(model);
+var formatter = new JsonFormatter(JsonFormatter.Settings.Default);
+var json = formatter.Format(model);
 var jsonContent = new StringContent(json, Encoding.UTF8, "application/json");
 await PostRequest(jsonContent, "application/json");
 
@@ -38,7 +39,8 @@ static async Task PostRequest(HttpContent content, string type)
 
         if (type == "application/protobuf")
         {
-            var model = Serializer.Deserialize<Model>(response.Content.ReadAsStream());
+            var model = new Model();
+            model.MergeFrom(response.Content.ReadAsStream());
             Console.WriteLine($"{model.Id}. {model.Data}");
         }
         else
