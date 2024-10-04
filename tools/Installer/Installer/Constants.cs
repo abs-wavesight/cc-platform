f6e4498bd8ac21371e184c802da424fd9d6d56db
Dmytro Hurov <DHurov@eagle.org>
2024-10-04 08:26:35 -0500

    Added shovel component

--- tools/Installer/Installer/Constants.cs
+++ tools/Installer/Installer/Constants.cs
@@ -6,4 +6,27 @@ public static class Constants
 
     public const string NugetEnvironmentVariableName = "ABS_NUGET_PASSWORD";
     public const string AbsHeaderValue = "ABS";
+
+    public static class Headers
+    {
+        public const string ClientTenant = "cc-clienttenantid";
+        public const string PartnerTenant = "cc-partnertenantid";
+        public const string Tenant = "cc-tenantid";
+        public const string Client = "cc-clientid";
+        public const string ImoNumber = "cc-imo_number";
+        public const string VoyageId = "cc-voyage_id";
+        public const string Accept = "Accept";
+        public const string ContentType = "Content-Type";
+    }
+
+    public static class MediaTypes
+    {
+        public const string Any = "*/*";
+
+        public const string AnyProtobuf = "*/protobuf";
+        public const string Protobuf = "application/protobuf";
+
+        public const string AnyJson = "*/json";
+        public const string Json = "application/json";
+    }
 }
