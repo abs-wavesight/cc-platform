f75b7ae6bc41ba39727fe0a886fd7f45dde873c7
Yevhenii Lysianyi <YLysianyi@eagle.org>
2024-10-17 17:24:12 -0500

    Return message publisher interface

---
+++ libraries/Drex.Shared/Shared/MessageBus/Publish/IMessagePublisher.cs
@@ -0,0 +1,8 @@
+﻿namespace Abs.CommonCore.Drex.Shared.MessageBus.Publish;
+
+public interface IMessagePublisher
+{
+    public string BusKey { get; }
+
+    Task PublishAsync(object message, string destinationQueueName, Dictionary<string, string> headers);
+}
