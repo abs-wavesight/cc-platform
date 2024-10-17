c092e8252b3c5f41b3e9f0ce23423a62609b13ec
Yevhenii Lysianyi <140668615+YLysianyi@users.noreply.github.com>
2024-10-17 16:28:05 -0500

    Remove rebus nuget package (#287)

--- libraries/Drex.Shared/Shared/MessageBus/Publish/IMessagePublisher.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Abs.CommonCore.Drex.Shared.MessageBus.Publish;
-
-public interface IMessagePublisher
-{
-    public string BusKey { get; }
-
-    Task PublishAsync(object message, string destinationQueueName, Dictionary<string, string> headers);
-}
