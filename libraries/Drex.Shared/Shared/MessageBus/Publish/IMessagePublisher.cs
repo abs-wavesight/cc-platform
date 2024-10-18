2110010513232773d880491d5d2a65890e75c034
Yevhenii Lysianyi <140668615+YLysianyi@users.noreply.github.com>
2024-10-18 10:19:02 -0500

    Feature/ado 128271 rebus remove (#288)
    * Remove rebus nuget package
    
    * Return message publisher interface
    
    * Add IMessagePublisher
    
    * Update installer nugets
    
    * Change DataRequestService to return stream
    
    * Handle invalid github release download

---
+++ libraries/Drex.Shared/Shared/MessageBus/Publish/IMessagePublisher.cs
@@ -0,0 +1,6 @@
+﻿namespace Abs.CommonCore.Drex.Shared.MessageBus.Publish;
+public interface IMessagePublisher
+{
+    public string BusKey { get; }
+    Task PublishAsync(object message, string destinationQueueName, Dictionary<string, string> headers);
+}
