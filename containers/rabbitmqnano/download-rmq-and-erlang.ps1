350eb65373e8e58a2a5a9591640de5ec891e1557
Dmytro Hurov <DHurov@eagle.org>
2025-01-02 17:29:18 -0600

    Updated arguments

--- containers/rabbitmqnano/download-rmq-and-erlang.ps1
+++ containers/rabbitmqnano/download-rmq-and-erlang.ps1
@@ -1,6 +1,6 @@
 cd ".\containers\rabbitmqnano"
 Invoke-WebRequest -Uri "https://erlang.org/download/otp_win64_25.1.1.exe" -OutFile ".\erlang_install.exe"
-Start-Process -Wait -FilePath ".\erlang_install.exe" -ArgumentList /S, /D=".\erlang\"
+Start-Process -Wait erlang_install.exe -ArgumentList "/S /D=.\erlang\"
 dir
 #Remove-Item -Force -Path ".\erlang_install.exe"
 Invoke-WebRequest -Uri "https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.8.15/rabbitmq-server-windows-3.8.15.zip" -OutFile ".\rabbitmq.zip"
