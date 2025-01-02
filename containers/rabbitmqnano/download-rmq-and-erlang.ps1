8f1c7c7215d4f2a4f57d28e7c8a8d3f1d6f9c343
Dmytro Hurov <DHurov@eagle.org>
2025-01-02 16:41:38 -0600

    Updated paths in PS script

--- containers/rabbitmqnano/download-rmq-and-erlang.ps1
+++ containers/rabbitmqnano/download-rmq-and-erlang.ps1
@@ -1,8 +1,9 @@
 Invoke-WebRequest -Uri "https://erlang.org/download/otp_win64_25.1.1.exe" -OutFile ".\erlang_install.exe"
-Start-Process -Wait -FilePath ".\erlang_install.exe" -ArgumentList /S, /D=.\erlang\
-Remove-Item -Force -Path ".\erlang_install.exe"
+Start-Process -Wait -FilePath ".\erlang_install.exe" -ArgumentList /S, /D=".\containers\rabbitmqnano\erlang\"
+#Remove-Item -Force -Path ".\erlang_install.exe"
 Invoke-WebRequest -Uri "https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.8.15/rabbitmq-server-windows-3.8.15.zip" -OutFile ".\rabbitmq.zip"
-Expand-Archive -Path ".\rabbitmq.zip" -DestinationPath ".\"
+Expand-Archive -Path ".\rabbitmq.zip" -DestinationPath ".\containers\rabbitmqnano"
 Remove-Item -Force -Path ".\rabbitmq.zip"
-Rename-Item -Path ".\rabbitmq_server-3.8.15" -NewName ".\rabbitmq"
+Rename-Item -Path ".\containers\rabbitmqnano\rabbitmq_server-3.8.15" -NewName ".\containers\rabbitmqnano\rabbitmq"
+cd ".\containers\rabbitmqnano"
 dir
