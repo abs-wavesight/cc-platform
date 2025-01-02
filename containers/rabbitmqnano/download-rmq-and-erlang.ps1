0a0b195925934f0142ff4e9bc8a0fc137f2af163
Dmytro Hurov <DHurov@eagle.org>
2025-01-02 16:12:41 -0600

    removed rmq and erlang files

---
+++ containers/rabbitmqnano/download-rmq-and-erlang.ps1
@@ -0,0 +1,7 @@
+Invoke-WebRequest -Uri "https://erlang.org/download/otp_win64_25.1.1.exe" -OutFile ".\erlang_install.exe"
+Start-Process -Wait -FilePath ".\erlang_install.exe" -ArgumentList /S, /D=.\erlang\
+Remove-Item -Force -Path ".\erlang_install.exe"
+Invoke-WebRequest -Uri "https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.8.15/rabbitmq-server-windows-3.8.15.zip" -OutFile ".\rabbitmq.zip"
+Expand-Archive -Path ".\rabbitmq.zip" -DestinationPath ".\"
+Remove-Item -Force -Path ".\rabbitmq.zip"
+Rename-Item -Path ".\rabbitmq_server-3.8.15" -NewName ".\rabbitmq"
