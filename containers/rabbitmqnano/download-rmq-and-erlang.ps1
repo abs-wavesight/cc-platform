77adbc566baebb9274d28973b68bf835955726e3
Dmytro Hurov <DHurov@eagle.org>
2025-01-03 08:12:15 -0600

    Added plug-in download

--- containers/rabbitmqnano/download-rmq-and-erlang.ps1
+++ containers/rabbitmqnano/download-rmq-and-erlang.ps1
@@ -1,5 +1,5 @@
 cd ".\containers\rabbitmqnano"
-Invoke-WebRequest -Uri "https://erlang.org/download/otp_win64_25.1.1.exe" -OutFile ".\erlang_install.exe"
+Invoke-WebRequest -Uri "https://erlang.org/download/otp_win64_22.3.exe" -OutFile ".\erlang_install.exe"
 Start-Process -Wait erlang_install.exe -ArgumentList "/S /D=D:\a\cc-platform\cc-platform\containers\rabbitmqnano\erlang\"
 dir
 #Remove-Item -Force -Path ".\erlang_install.exe"
@@ -8,4 +8,5 @@ Expand-Archive -Path ".\rabbitmq.zip" -DestinationPath ".\"
 Remove-Item -Force -Path ".\rabbitmq.zip"
 dir
 Rename-Item -Path ".\rabbitmq_server-3.8.15" -NewName ".\rabbitmq"
+Invoke-WebRequest -Uri "https://github.com/rabbitmq/rabbitmq-delayed-message-exchange/releases/download/3.8.9/rabbitmq_delayed_message_exchange-3.8.9-0199d11c.ez" -OutFile ".\rabbitmq\plugins\rabbitmq_delayed_message_exchange-3.8.9.ez"
 dir
