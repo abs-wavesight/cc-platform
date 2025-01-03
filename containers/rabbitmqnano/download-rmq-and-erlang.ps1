048c54c2d26b0fefa625caf8313be8b2084d6f7b
Dmytro Hurov <DHurov@eagle.org>
2025-01-03 08:44:51 -0600

    Updated user permissions in docker

--- containers/rabbitmqnano/download-rmq-and-erlang.ps1
+++ containers/rabbitmqnano/download-rmq-and-erlang.ps1
@@ -1,8 +1,7 @@
 cd ".\containers\rabbitmqnano"
 Invoke-WebRequest -Uri "https://erlang.org/download/otp_win64_22.3.exe" -OutFile ".\erlang_install.exe"
 Start-Process -Wait erlang_install.exe -ArgumentList "/S /D=D:\a\cc-platform\cc-platform\containers\rabbitmqnano\erlang\"
-dir
-#Remove-Item -Force -Path ".\erlang_install.exe"
+Remove-Item -Force -Path ".\erlang_install.exe"
 Invoke-WebRequest -Uri "https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.8.15/rabbitmq-server-windows-3.8.15.zip" -OutFile ".\rabbitmq.zip"
 Expand-Archive -Path ".\rabbitmq.zip" -DestinationPath ".\"
 Remove-Item -Force -Path ".\rabbitmq.zip"
