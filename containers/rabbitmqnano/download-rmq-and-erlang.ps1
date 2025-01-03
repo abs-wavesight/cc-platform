b9932d4b9026ca460a4a4f92b5ee7e5e80d81517
Dmytro Hurov <DHurov@eagle.org>
2025-01-03 08:24:30 -0600

    added debug logs

--- containers/rabbitmqnano/download-rmq-and-erlang.ps1
+++ containers/rabbitmqnano/download-rmq-and-erlang.ps1
@@ -9,4 +9,7 @@ Remove-Item -Force -Path ".\rabbitmq.zip"
 dir
 Rename-Item -Path ".\rabbitmq_server-3.8.15" -NewName ".\rabbitmq"
 Invoke-WebRequest -Uri "https://github.com/rabbitmq/rabbitmq-delayed-message-exchange/releases/download/3.8.9/rabbitmq_delayed_message_exchange-3.8.9-0199d11c.ez" -OutFile ".\rabbitmq\plugins\rabbitmq_delayed_message_exchange-3.8.9.ez"
+cd ".\erlang"
+dir
+cd ".\erts-10.7\bin"
 dir
