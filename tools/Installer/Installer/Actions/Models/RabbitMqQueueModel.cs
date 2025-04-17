a44f8ea11fbed0dee66d3ca9d21641be36c33f35
DmytroHurov <135887254+DmytroHurov@users.noreply.github.com>
2025-04-17 10:29:35 -0500

    Updated Installer (#338)
    * Updated Installer
    
    * Fixed Proj file
    
    * Fixed tests
    
    * Fixed unit tests
    
    * Temporary disabled integration test
    
    * Fixed installer tests
    
    * Fixed code formatting
    
    * Updated unit test
    
    * Updated unit test

---
+++ tools/Installer/Installer/Actions/Models/RabbitMqQueueModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Abs.CommonCore.Installer.Actions.Models;
+
+public class RabbitMqQueueModel
+{
+    [JsonPropertyName("name")]
+    public string Name { get; set; } = string.Empty;
+    [JsonPropertyName("vhost")]
+    public string VHost { get; set; } = string.Empty;
+    [JsonPropertyName("durable")]
+    public bool Durable { get; set; }
+    [JsonPropertyName("auto_delete")]
+    public bool AutoDelete { get; set; }
+    [JsonPropertyName("arguments")]
+    public Dictionary<string, object> Arguments { get; set; } = new();
+}
